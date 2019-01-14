5bfb3d69c09d2de6d1f6053184155feca5dcd72d
Ogo Anaekwe <ogonnaanaekwe@Ogos-MacBook-Air.local>
2019-01-14 16:25:32 -0500

    feat(fav): add favorite logic

--- src/components/TruckCard/TruckCard.js
+++ src/components/TruckCard/TruckCard.js
@@ -1,40 +1,141 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
 import Typography from '@material-ui/core/Typography';
+// import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import Favorite from '@material-ui/icons/Favorite';
+import AddBox from '@material-ui/icons/AddBox';
+import PhonelinkRing from '@material-ui/icons/PhonelinkRing';
+import Email from '@material-ui/icons/Email';
 
-function TruckCard() {
+const styles = {
+  card: {
+    maxWidth: 345,
+    borderTopRightRadius: 15,
+    borderTopLeftRadius: 15,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    marginTop: 70,
+    boxShadow: '0px 5px 7px 5px rgba(0, 0, 255, 0.15)',
+  },
+  media: {
+    height: 250,
+  },
+  link: {
+    textDecoration: 'none',
+    color: 'rgba(0, 0, 0, 0.87)',
+  },
+  button: {
+    borderRadius: 15,
+    boxShadow: '0px 5px 7px 5px rgba(0, 0, 255, 0.06)',
+  },
+};
+
+function TruckCard(props) {
+  const { classes, truck, setFavorite } = props;
+  // const [favorite, setFavorite] = useState(truck.favorite);
+  const [openSnack, setopenSnack] = useState(false);
+  // const handleFavorite = () => {
+  //   setFavorite(!favorite);
+  //   setopenSnack(true);
+  //   console.log(truck);
+  // };
+  const handleCloseSnack = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setopenSnack(false);
+  };
   return (
-  <Card>
+    <Card className={classes.card}>
       <CardActionArea>
         <CardMedia
-          src="https://via.placeholder.com/150"
-          title="Truck"
+          className={classes.media}
+          image={truck.image}
+          title={`${truck.name} going for ${truck.price}`}
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Super Truck
+          <Typography gutterBottom variant="overline" component="h2">
+          {truck.name}
+          </Typography>
+          <Typography gutterBottom variant="caption" component="h2">
+            ${truck.price}
           </Typography>
           <Typography component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
+            {truck.description}
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
-          View Details
+        <Link className={classes.link} to={`/details?id=${truck.id}`}>
+          <Button className={classes.button} size="small" color="primary">
+            <AddBox />
+          </Button>
+        </Link>
+        <Button
+          className={classes.button}
+          onClick={() => {
+            setFavorite(truck.id);
+            setopenSnack(!openSnack);
+            console.log(truck);
+          }
+          }
+          disabled={truck.favorite}
+          size="small"
+          color="primary"
+        >
+          <Favorite />
+          <Snackbar
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            open={openSnack}
+            autoHideDuration={3000}
+            onClose={handleCloseSnack}
+            ContentProps={{
+              'aria-describedby': 'message-id',
+            }}
+            message={<span id="message-id">{truck.name} added to favorites!</span>}
+          />
         </Button>
-        <Button size="small" color="primary">
-         Add To Wishlist
-        </Button>
+        <Button
+            aria-haspopup="true"
+            className={classes.button}
+            size="small"
+            color="primary"
+            href={`tel:${truck.contact_info.phone_number}`}
+          >
+            <PhonelinkRing />
+          </Button>
+          <Button
+            aria-haspopup="true"
+            className={classes.button}
+            size="small"
+            color="primary"
+            href={`mailto:${truck.contact_info.email}`}
+          >
+            <Email />
+          </Button>
       </CardActions>
     </Card>
   );
 }
 
-export default TruckCard;
+
+TruckCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+  truck: PropTypes.object.isRequired,
+  setFavorite: PropTypes.func.isRequired,
+};
+
+export default withStyles(styles)(TruckCard);
--- src/routes/trucks/Trucks.js
+++ src/routes/trucks/Trucks.js
@@ -1,24 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import { saveState, loadState } from '../../tools/localStorage';
 import TruckCard from '../../components/TruckCard/TruckCard';
+import trucksData from '../../data.json';
 
-function Trucks() {
+const styles = {
+  card: {
+    // maxWidth: 345,
+    flexGrow: 1,
+    marginLeft: 87,
+  },
+  media: {
+    height: 140,
+  },
+  link: {
+    textDecoration: 'none',
+  },
+};
+
+function Trucks(props) {
+  const [trucks, setTrucks] = useState([]);
+  const fetchData = () => {
+    setTrucks(trucksData);
+  };
+  // Store trucks to local storage
+  useEffect(() => {
+    if (trucks.length > 1) {
+      saveState(trucks);
+    }
+  }, [trucks]);
+  // Load trucks from cache
+  useEffect(() => {
+    const trucksFromMemory = loadState();
+    // Load trucks from Session Storage if available
+    if (trucksFromMemory) {
+      setTrucks(trucksFromMemory);
+    } else {
+      fetchData();
+    }
+  }, []);
+  const { classes } = props;
   return (
-    <div>
-      <h1>Hello Trucks</h1>
-      <Grid container spacing={16}>
-        <Grid item xs={4}>
-          <TruckCard />
-        </Grid>
-        <Grid item xs={4}>
-          <TruckCard />
-        </Grid>
-        <Grid item xs={4}>
-          <TruckCard />
-        </Grid>
+    <div className={classes.card}>
+      <Grid container spacing={8}>
+          {trucks.map((truck, index) => (
+            <Grid key={index} item xs={4}>
+            <TruckCard
+              truck={truck}
+              setFavorite={(id) => {
+                trucks.forEach((elem) => {
+                  if (elem.id === id) {
+                    // eslint-disable-next-line no-param-reassign
+                    elem.favorite = true;
+                  }
+                });
+                saveState(trucks);
+              }}
+            />
+            </Grid>
+          ))}
       </Grid>
     </div>
   );
 }
 
-export default Trucks;
+Trucks.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Trucks);
